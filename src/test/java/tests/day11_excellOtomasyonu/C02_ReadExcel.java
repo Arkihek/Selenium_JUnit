94282dbe45475eec69d784e27c3235cf4e8af496
ihsan YILMAZ <arkihekQA@gmail.com>
2023-07-05 12:54:31 +0300

    Day 11

---
+++ src/test/java/tests/day11_excellOtomasyonu/C02_ReadExcel.java
@@ -0,0 +1,75 @@
+package tests.day11_excellOtomasyonu;
+
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class C02_ReadExcel {
+    @Test
+    public void test01() throws IOException {
+        String dosyaYolu = "src/test/java/tests/day11_excellOtomasyonu/ulkeler.xlsx";
+        FileInputStream fis = new FileInputStream(dosyaYolu);
+        Workbook workbook = WorkbookFactory.create(fis);
+        Sheet sayfa1 = workbook.getSheet("Sayfa1");
+
+        // - 1.satirdaki 2.hucreye gidelim ve yazdiralim
+        System.out.println(sayfa1.getRow(0).getCell(1)); // Başkent (İngilizce)
+        // - 1.satirdaki 2.hucreyi bir string degiskene atayalim ve yazdiralim
+        String istenenHucre = sayfa1.getRow(0).getCell(1).toString(); // Başkent (İngilizce)
+        System.out.println(istenenHucre);
+        // - 2.satir 4.cell’in afganistan’in baskenti Kabil oldugunu test edelim
+        String expectedYazi = "Kabil";
+        String actualYazi = sayfa1.getRow(1).getCell(3).toString();
+        Assert.assertEquals(expectedYazi, actualYazi);
+
+        // - Satir sayisini bulalim
+        System.out.println(sayfa1.getLastRowNum()); // 190 yani index
+
+        // - Fiziki olarak kullanilan satir sayisini bulun
+        System.out.println(sayfa1.getPhysicalNumberOfRows());// 191 fiziki olarak kullanilan satiri buluruz
+
+        // Sayfa 2 icin son satir numarasi ve fiziki olarak kullanilan satir sayisini yazdirin
+
+        Sheet sayfa2 = workbook.getSheet("Sayfa2");
+        System.out.println("Son satir indexi : " + sayfa2.getLastRowNum()); // 23
+        System.out.println("fiziki olarak kullanilan satir sayisi: " + sayfa2.getPhysicalNumberOfRows()); // 8
+
+        // Listede Togo isminde bir ulke oldugunu test edin
+        boolean flag = false;
+        for (int i = 0; i <= sayfa1.getLastRowNum(); i++) {
+            String satirdakiUlkeIsmi = sayfa1.getRow(i).getCell(0).toString();
+
+            if (satirdakiUlkeIsmi.equals("Togo")) {
+                flag = true;
+                System.out.println("Excell`de Togo var");
+                break;
+            }
+        }
+        Assert.assertTrue(flag);
+
+        // - Ingilizce Ulke isimleri ve baskentleri bir map olarak kaydedelim
+        Map<String,String> ulkeIsimleriMap = new HashMap<>();
+        for (int i = 0; i <= sayfa1.getLastRowNum(); i++) {
+            String key = sayfa1.getRow(i).getCell(0).toString();
+            String value = sayfa1.getRow(i).getCell(1).toString();
+            ulkeIsimleriMap.put(key,value);
+        }
+        System.out.println(ulkeIsimleriMap);
+
+        // Listede Benin isminde bir ulke bulundugunu test edin
+        Assert.assertTrue(ulkeIsimleriMap.containsKey("Benin"));
+
+        // Listede Ankara isminde bir baskent bulundugunu test edin
+        Assert.assertTrue(ulkeIsimleriMap.containsValue("Ankara"));
+    }
+
+}
---
+++ src/test/java/tests/day11_excellOtomasyonu/C03_WriteExcel.java
@@ -0,0 +1,10 @@
+package tests.day11_excellOtomasyonu;
+
+import org.junit.Test;
+
+public class C03_WriteExcel {
+    @Test
+    public void test01(){
+
+    }
+}
